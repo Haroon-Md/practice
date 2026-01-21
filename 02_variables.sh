017a3b4646b95809d083a4be1d007d93b57759e9
Mohammad Haroonbasha <haroonbashamohammad@gmail.com>
2026-01-21 12:17:48 +0530

    changed file name

---
+++ 02_variables.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$person1::sees the $person2 "
+echo "$person2::hii raa nanna"
+echo "$person1::mundhu kaal thi raa pookaaa"
+echo "$person2:sarlevoii edho friendly ga esaam"
