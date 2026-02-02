91c22dc5cda0a7bc810703582d908f341fd2de74
haroon basha mohammad <haroonbashamohammad@gmail.com>
2026-02-02 15:53:15 +0530

    test

--- roboshop.sh
+++ roboshop.sh
@@ -3,10 +3,10 @@
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-0d7cfee2d46161f43"
 
-for instances in $@
+for instance in $@
 do
-	 INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications"ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
-	if [ $instance !=frontend ];then
+	 INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+	if [[ $instance != frontend ]];then
 	IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
         RECORD_NAME="$DOMAIN_NAME"
 	else
